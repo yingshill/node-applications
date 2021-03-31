ecbbee22165a832ac45b4cab315f4222c43b4873
Yingshi Liu <serendipper.liu@gmail.com>
2021-03-31 16:04:52 -0700

    set up add for heroku

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -8,7 +8,7 @@ messageOne.textContent = ""
 weatherForm.addEventListener("submit", (e) => {
     e.preventDefault()
     const location = search.value
-    const url = "http://localhost:3000/weather?address=" + location
+    const url = "/weather?address=" + location
     messageOne.textContent = "Loading..."
     fetch(url).then((response) => {
         response.json().then((data) => {
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const express = require("express")
 const app = express()
+const port = process.env.PROT || 3000
 const hbs = require("hbs")
 const forecast = require("./utils/forecast")
 const geocode = require("./utils/geocode")
@@ -86,6 +87,6 @@ app.get("*", (req, res) => {
         errorMessage: "404 Not Found"
     })
 })
-app.listen(3000, ()=> {
-    console.log("server is up running.")
+app.listen(port, ()=> {
+    console.log("server is up on port " + port)
 })
