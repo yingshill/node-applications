5f917f88812de7bd1d583ecdd45b6a8aa819eb02
Yingshi Liu <serendipper.liu@gmail.com>
2021-03-31 17:34:16 -0700

    update src/app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const express = require("express")
 const app = express()
-const port = process.env.PROT || 3000
+const port = process.env.PORT || 3000
 const hbs = require("hbs")
 const forecast = require("./utils/forecast")
 const geocode = require("./utils/geocode")
